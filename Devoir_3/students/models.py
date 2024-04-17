39a2f1c3a7ecc08c398f6377d0aad417bdb1ea28
Franck Tiomo <einstein-franck.tiomo-epongo@polymtl.ca>
2024-04-17 15:18:18 -0400

    problem 2 done

--- Devoir_3/students/models.py
+++ Devoir_3/students/models.py
@@ -66,6 +66,12 @@ class RegressionModel(object):
     def __init__(self) -> None:
         # Initialize your model parameters here
         "*** TODO: COMPLETE HERE FOR QUESTION 2 ***"
+        self.num_layers = 3
+        self.batch_size = 1
+        self.learning_rate = 0.001
+        self.w = [nn.Parameter(1, 10), nn.Parameter(10, 10), nn.Parameter(10, 1)]
+        self.b = [nn.Parameter(1, 10), nn.Parameter(1, 10), nn.Parameter(1, 1)]
+
 
     def run(self, x: nn.Constant) -> nn.Node:
         """
@@ -77,6 +83,13 @@ class RegressionModel(object):
             A node with shape (batch_size x 1) containing predicted y-values
         """
         "*** TODO: COMPLETE HERE FOR QUESTION 2 ***"
+        h = x
+        for i in range(self.num_layers):
+            if i == self.num_layers - 1: # Not applying ReLU for the last layer
+                h = nn.AddBias(nn.Linear(h, self.w[i]), self.b[i])
+            else:
+                h = nn.ReLU(nn.AddBias(nn.Linear(h, self.w[i]), self.b[i]))
+        return h
 
     def get_loss(self, x: nn.Constant, y: nn.Constant) -> nn.Node:
         """
@@ -89,12 +102,31 @@ class RegressionModel(object):
         Returns: a loss node
         """
         "*** TODO: COMPLETE HERE FOR QUESTION 2 ***"
+        return nn.SquareLoss(self.run(x), y)
 
     def train(self, dataset: RegressionDataset) -> None:
         """
         Trains the model.
         """
         "*** TODO: COMPLETE HERE FOR QUESTION 2 ***"
+
+        while True:
+            has_errors = False
+            for x, y in dataset.iterate_once(self.batch_size):
+                loss = self.get_loss(x, y)
+                if nn.as_scalar(loss) > 0.02:
+                    has_errors = True
+                    while nn.as_scalar(loss) > 0.02:
+                        grad_w_0, grad_b_0, grad_w_1, grad_b_1, grad_w_2, grad_b_2 = nn.gradients(loss, [self.w[0], self.b[0], self.w[1], self.b[1], self.w[2], self.b[2]])
+                        self.w[0].update(grad_w_0, -self.learning_rate)
+                        self.b[0].update(grad_b_0, -self.learning_rate)
+                        self.w[1].update(grad_w_1, -self.learning_rate)
+                        self.b[1].update(grad_b_1, -self.learning_rate)
+                        self.w[2].update(grad_w_2, -self.learning_rate)
+                        self.b[2].update(grad_b_2, -self.learning_rate)
+                        loss = self.get_loss(x, y)
+            if not has_errors:
+                break
 
 
 class DigitClassificationModel(object):
